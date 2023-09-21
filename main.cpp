07852c880c420cf8f9708e5bec88a2f50fc8087d
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2023-09-21 13:50:05 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,27 +3,30 @@
 #include <math.h>
 //#include <dir.h>
 #include <string> // for string type
+#include <stdlib.h>
 #define LIMIT 255 // предел имени файла/директории
 
+// #include <iostream>
+// using std::cout;
 using std::string;
 
-string NameGenerate(int i, string Alphabet) {
+string NameGenerate(int iterrator, string Alphabet) {
     string result = "";
-    int CursorValue = Alphabet.length()%i;
+    int LengthStr = (int)Alphabet.length();
+    int CursorValue = 0;
+    CursorValue = LengthStr/(iterrator+1);
     char Cursor = Alphabet[CursorValue];
-    for(int j =0; j < CursorValue; j++) {
-        result += Alphabet[j]; 
+    srand(time(NULL));
+    int random;
+    for (int i = 0; i < LengthStr; i++) {
+        random = (rand()/((LIMIT-4)/LengthStr)) + 2;
+        for (int j = 0; j < random; j++) {
+            result += Alphabet[i];
+        }
     }
-    for (int j = 0;j < 250-(Alphabet.length()-CursorValue); j++) {
-        result += Cursor;
-    }
-    for(int j = 0; j < Alphabet.length()-CursorValue; j++) {
-        result += Alphabet[j];
-    }
-    return result;
 }
 void FolderCreate(int Count, string Alphabet) {
-    FILE *Log = fopen("LOG.log", "wa");
+    FILE *Log = fopen("LOG.txt", "a");
     string NameFolder;
     time_t tm;
     for(int i = 0; i < Count; i++) {
@@ -32,9 +35,24 @@ void FolderCreate(int Count, string Alphabet) {
         tm = time(NULL);
         fprintf(Log, "Folder %s create on %s\n", (char*)NameFolder.c_str(), ctime(&tm));
     }
+    fclose(Log);
 }
 void FileCreate(int Count, string Alphabet) {
-    
+    FILE *Log = fopen("LOG.txt", "a");
+    FILE *newF;
+    string NameFile;
+    string FullNameFile;
+    time_t tm;
+    for(int i = 0; i < Count; i++) {
+        NameFile = NameGenerate(i, Alphabet);
+        FullNameFile = Alphabet[0] + NameFile + ".txt";printf("test: %s", NameFile.c_str());
+        newF = fopen((char*)FullNameFile.c_str(), "w");
+        fprintf(newF, "test\n");printf(" 2...\n");
+        fclose(newF);
+        tm = time(NULL);
+        
+        fprintf(Log, "File %s create on %s\n", (char*)NameFile.c_str(), ctime(&tm));
+    }
 }
 int main() {
     int CountOfFolders, CountOfFiles;
@@ -42,7 +60,7 @@ int main() {
     char FilesAlphabet[LIMIT-4];
     string StFoldersAlphabet = "", StFilesAlphabet = ""; // string
     bool IsCorrectEnter = false;
-    do {
+    do {   //FoldersAlphabet
         printf("Enter folders alphabet: ");
         scanf("%s", FoldersAlphabet);
         StFoldersAlphabet = FoldersAlphabet;
@@ -58,13 +76,38 @@ int main() {
         scanf("%*[^\n]");
     }while(!IsCorrectEnter);
     IsCorrectEnter = false;
-    do {
+    do { // CountOfFolders
         printf("Enter Folders count: ");
         if (scanf("%i", &CountOfFolders) > 0 && CountOfFolders < tgamma(StFoldersAlphabet.length())*LIMIT) {
             IsCorrectEnter = true;
         }else printf("Invalid enter!\n");
         scanf("%*[^\n]");
     }while(!IsCorrectEnter);
-    
+    FolderCreate(CountOfFolders, StFoldersAlphabet);
+    do { // FilesAlphabet
+        printf("Enter files alphabet: ");
+        scanf("%s", FilesAlphabet);
+        StFilesAlphabet = FilesAlphabet;
+        IsCorrectEnter = true;
+        for (int i = 0; i < StFilesAlphabet.length(); i++) {
+            if (StFilesAlphabet[i] >= '0' && StFilesAlphabet[i] <= '9' || StFilesAlphabet[i] == ' ') {
+                IsCorrectEnter = false;
+            }
+        }
+        if(!IsCorrectEnter) {
+            printf("Invalid enter!\n");
+        }
+        scanf("%*[^\n]");
+    }while(!IsCorrectEnter);
+    IsCorrectEnter = false;
+    do {  // CountOfFiles
+        printf("Enter Files count: ");
+        if (scanf("%i", &CountOfFiles) > 0 && CountOfFiles < tgamma(StFilesAlphabet.length())*LIMIT) {
+            IsCorrectEnter = true;
+        }else printf("Invalid enter!\n");
+        scanf("%*[^\n]");
+    }while(!IsCorrectEnter);
+    printf("tess: %d\n", CountOfFiles);
+    FileCreate(CountOfFiles, FilesAlphabet);
     return 0;
 }
